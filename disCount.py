d44c26b7d5c4f67237585d365d0af4f2beefce1b
Tan Le <letan101104@gmail.com>
2024-04-19 15:42:01 -0400

    update some issues in matplotlib

--- disCount.py
+++ disCount.py
@@ -14,17 +14,21 @@ q = covariate / np.sum(covariate)
 
 
 N = 169 #total number of images
-human_verified_size = set({3, 5, 10, 15}) #choices of size of huma verfied samples
+human_verified_size = set({3, 5, 10}) #choices of size of huma verfied samples
 
 for k in human_verified_size:   
 #return some index sample from the distribution
     samples = list(np.random.choice(np.arange(N), k, p = q, replace = True))
     sampled_image = [dectetorResult[x]["image_id"] for x in samples]
     numpy_images = retreive_image(sampled_image)
-    fig, ax = plt.subplots(1, k, figsize = (20, 5 * k)) #create a Figure object with 3 slots
+    fig, ax = plt.subplots(1, k, figsize = (20, 10)) #create a Figure object with k slots, size is 20 inches width, 10 inches height
     for index, img in enumerate(numpy_images):
-        plt.subplot(1,k,index + 1) #register a slot for the same Figure object, last parameter is current index of plot(start with 1)
+        plt.subplot(1,k,index + 1) #retrieve a slot from the FIgiure object
         plt.imshow(img)
+        plt.axis("off")
     plt.show()
+
+    #TODO: get input of count for each image
+    f_s_i = []
 
 
