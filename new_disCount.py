cac863d30192d18623ea0c13497068083ea9ebfc
larissazhu123 <larissazhu@umass.edu>
2024-04-23 00:29:55 -0400

    mean error

--- new_disCount.py
+++ new_disCount.py
@@ -21,38 +21,49 @@ true_total_strawberries = groundTruth()
 F = true_total_strawberries
 
 #TODO: modify this code so that each k will be running with 100 trials, return (k, mean_of_all_error_trials)
-def run_dis_count(k):
+def run_dis_count(k, trials = 100):
+    error_rates = []
     print(f"______DISCOUNT RUN WITH K = {k}______")
-    samples = list(np.random.choice(np.arange(N), k, p = q, replace = True))
-    sampled_image = [dectetorResult[x]["image_id"] for x in samples]
+    for _ in range(trials):
+        samples = list(np.random.choice(np.arange(N), k, p = q, replace = True))
+        sampled_image = [dectetorResult[x]["image_id"] for x in samples]
+        
+        f_s_i = [int(dectetorResult[x]["true_count"]) for x in samples]
+
+        #!Formula to calculate error rate, CI are copied from Guastavo's tutorial
+        w_bar = 0
+        for i, s_i in enumerate(samples):
+            w_bar += f_s_i[i]/covariate.flatten()[s_i] # w_bar(S) = \sum(f/g)
+        F_hat = np.sum(covariate)*w_bar/len(samples) # count estimate: F_hat = G(S)*1/n*w_bar(S)
+        print('Estimated number of objects: %d (true count: %d)'%(F_hat, F))
+
+        # Calculating confidence intervals
+        w_ci = 0
+        for i, s_i in enumerate(samples):
+            w_ci += (np.sum(covariate)*f_s_i[i]/covariate.flatten()[s_i] - F_hat)**2
+        var_hat = w_ci/len(samples) # estimated variance
+        CI = 1.96*np.sqrt(var_hat/len(samples)) # 95% confidence intervals
+
+        err = np.abs(F-F_hat) / F
+        error_rates.append(err * 100)
+
+    # let's get the mean error
+    mean_error = np.mean(error_rates)
+    print(f"Mean Error rate for {k}:", mean_error)
+    return (k, mean_error)
     
-    f_s_i = [int(dectetorResult[x]["true_count"]) for x in samples]
-
-    #!Formula to calculate error rate, CI are copied from Guastavo's tutorial
-    w_bar = 0
-    for i, s_i in enumerate(samples):
-        w_bar += f_s_i[i]/covariate.flatten()[s_i] # w_bar(S) = \sum(f/g)
-    F_hat = np.sum(covariate)*w_bar/len(samples) # count estimate: F_hat = G(S)*1/n*w_bar(S)
-    print('Estimated number of objects: %d (true count: %d)'%(F_hat, F))
-
-    # Calculating confidence intervals
-    w_ci = 0
-    for i, s_i in enumerate(samples):
-        w_ci += (np.sum(covariate)*f_s_i[i]/covariate.flatten()[s_i] - F_hat)**2
-    var_hat = w_ci/len(samples) # estimated variance
-    CI = 1.96*np.sqrt(var_hat/len(samples)) # 95% confidence intervals
-
     # And here we calculate the error
-    Error = np.abs(F - F_hat)/F
-    print('Error rate: %.2f%%'%(Error*100.))
-    print('Estimate: %d \u00B1 %d'%(F_hat, CI))
-    return (k , Error * 100)
+    # Error = np.abs(F - F_hat)/F
+    # print('Error rate: %.2f%%'%(Error*100.))
+    # print('Estimate: %d \u00B1 %d'%(F_hat, CI))
+    # return (k , Error * 100)
 
 
 k_coordinates = []
 error_rate_coordiantes = []
 
 set_of_k_values = [3, 5, 10, 12, 15, 17, 21, 25, 27, 30, 33, 38, 41, 45, 48, 51, 54, 57, 60]
+
 
 for k in set_of_k_values:
     cur = run_dis_count(k)
